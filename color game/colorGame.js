fa31ed5c1b3564a2809131c1f7b8391a30fedd2c
Matt Palamos <mattpalamos@hotmail.com>
2017-06-06 00:03:58 -0700

    made it so the 6 colors are random and all the squares change to the correct color

--- color game/colorGame.js
+++ color game/colorGame.js
@@ -1,10 +1,12 @@
 
 
-var colors = ['rgb(255, 0, 0)', 'rgb(255, 255, 0)', 'rgb(0, 255, 0)', 'rgb(0, 0, 255)', 'rgb(255, 0, 0)', 'rgb(255, 0, 255)'];
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll('.square');
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById('colorDisplay');
+var messageDisplay = document.querySelector('#message');
+var h1 = document.querySelector('h1');
 
 colorDisplay.textContent = pickedColor;
 
@@ -17,11 +19,52 @@ for (var i = 0; i < squares.length; i++ ) {
     var clickedColor = (this.style.backgroundColor);
 
     if(clickedColor === pickedColor) {
-      alert('CORRECT');
+      messageDisplay.textContent = 'Correct';
+      changeColors(clickedColor);
+      h1.style.backgroundColor = clickedColor;
     } else {
-      alert('WRONG');
+      this.style.background ='#232323';
+      messageDisplay.textContent = 'Try Again';
     }
-
   });
   //style.backgroundColor}
 }
+
+
+function changeColors(color) {
+  //loop thru squares
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = color;
+  }
+}
+
+function pickColor() {
+  var random = Math.floor(Math.random() * colors.length);
+  return colors[random];
+}
+
+function generateRandomColors(num) {
+
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push(randomColor())
+  }
+
+  return arr;
+}
+
+
+function randomColor() {
+  //pick a red
+  var r = Math.floor(Math.random() * 256);
+  //pick a green
+  var g = Math.floor(Math.random() * 256);
+  //a blue from 0-255
+  var b = Math.floor(Math.random() * 256);
+  'rgb(r, g, b)'
+
+  return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+
+}
+
